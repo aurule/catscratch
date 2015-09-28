2dee38306ea00d41a1c34743d2bf780bf910bbff
Peter Andrews <peter.andrews@modolabs.com>
2015-09-28 11:46:51 -0400

    Working signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            # do things
+            flash[:success] = "Welcome to the sample app! Enjoy its new and improved lack of functionality."
+            redirect_to @user
         else
             render 'new'
         end
